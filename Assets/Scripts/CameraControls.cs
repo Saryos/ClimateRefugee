8d22e050b4736aa87e487e650eb174a29f333982
Joni Räsänen <joni.rasanen@iki.fi>
2019-01-26 15:25:51 +0200

    Created prefabs for the levelobjects. A quick camera movement script.

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+
+    public int cameraSpeed_ = 10;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0, Time.deltaTime * cameraSpeed_, 0);
+        }
+        if (Input.GetKey("down") || Input.GetKey("s"))
+        {
+            transform.position += new Vector3(0, -Time.deltaTime * cameraSpeed_, 0);
+        }
+        if (Input.GetKey("left") || Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-Time.deltaTime * cameraSpeed_, 0, 0);
+        }
+        if (Input.GetKey("right") || Input.GetKey("d"))
+        {
+            transform.position += new Vector3(Time.deltaTime * cameraSpeed_, 0, 0);
+        }
+    }
+}
