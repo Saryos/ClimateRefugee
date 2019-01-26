bbcc83bbce92eb2055cf1f9bdf172fe17a39514f
Ear <tosiear@gmail.com>
2019-01-26 13:51:57 +0200

    stuff

---
+++ Assets/Scripts/gameState.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameState : MonoBehaviour{
+
+	enum DISASTERS {HURRICANE, FIRE, FLOOD, QUAKE, ARMAGEDDON};
+	enum RESOURCES {WOOD, ROCK};
+	// defenses: against single disaster or multiple?
+	enum DEFENCES {WALL, ROOF, DAM, CLEARANCE}
+	enum IMPROVEMENTS {INSULATION, ENERGY, OVEN}
+
+
+	double[] dangerLevels = new double[ System.Enum.GetNames(typeof(DISASTERS)).Length];
+	int[] resourceLevels = new int[System.Enum.GetNames(typeof(RESOURCES)).Length];
+	int[] defenceLevels = new int[System.Enum.GetNames(typeof(DEFENCES)).Length];
+	int[] improvementLevels = new int[System.Enum.GetNames(typeof(IMPROVEMENTS)).Length];
+
+	void addResource (RESOURCES resurssi, int amount){
+		resourceLevels[(int)resurssi] += amount;
+		// adjust dangerLevels
+	}
+
+	void buildDefence(DEFENCES building){
+		//adjust resources
+	}
+
+	void nightfall (){
+		// check survival
+		// adjust dangerLevels if multilevel?
+		// destroy resources
+		// lose house objects? If defense not enought?
+	}
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/gameboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameboard : MonoBehaviour{
+
+	const int xtiles = 40;
+	const int ytiles = 40;
+
+	// catastrophes
+	int fire = 0;
+	int hurricane = 0;
+	int earthquake = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		int[,] board = new int[xtiles,ytiles];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
