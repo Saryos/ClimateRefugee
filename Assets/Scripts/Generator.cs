ebb6042476ca0e01ca1ec9eaa4c2733d3ced52f8
Joni Räsänen <joni.rasanen@iki.fi>
2019-01-26 15:56:38 +0200

    Updated generation algorithm to include beaches.

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -9,6 +9,7 @@ public class Generator : MonoBehaviour
 
     public Tile grassTile_;
     public Tile waterTile_;
+    public Tile sandTile_;
 
     public int levelWidth_ = 40;
     public int levelHeight_ = 40;
@@ -18,25 +19,42 @@ public class Generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        generateScenario(levelWidth_, levelHeight_, 1, 1);
+        generateScenario(levelWidth_, levelHeight_, 0.0f, 0.0f);
     }
 
-    void generateScenario(int width, int height, int dryOffset, int heightOffset)
+    void generateScenario(int width, int height, float dryOffset, float heightOffset)
     {
         for (int i = 0; i < height; ++i)
         {
             for (int j = 0; j < width; ++j)
             {
+                double heightValue = noise_.Evaluate(j*0.05, i* 0.05); 
+                double drynessValue = noise_.Evaluate(j * 0.03 + 2250, i * 0.03 + 2250);
 
-                double tileValue = noise_.Evaluate(j*0.05, i*0.05);
-                if (tileValue > 0.05)
+                if (heightValue > -0.5 ) // too high for water
                 {
-                    tilemap_.SetTile(new Vector3Int(i, j, 0), grassTile_);
+                    if (heightValue < -0.3 ||drynessValue > 0.5) // close to water or too dry
+                    {
+                        tilemap_.SetTile(new Vector3Int(i, j, 0), sandTile_);
+                    }
+                    else
+                    {
+                        tilemap_.SetTile(new Vector3Int(i, j, 0), grassTile_);
+                    }
                 }
                 else
                 {
-                    tilemap_.SetTile(new Vector3Int(i, j, 0), waterTile_);
+                    if (drynessValue > 0.5) // no water if too dry
+                    {
+                        tilemap_.SetTile(new Vector3Int(i, j, 0), sandTile_);
+                    }
+                    else
+                    {
+                        tilemap_.SetTile(new Vector3Int(i, j, 0), waterTile_);
+                    }
                 }
+
+
 
 
                 //switch (tile)
