50ed47f0b5c17c93a7a6819e61d45912e15a413d
Joni Räsänen <joni.rasanen@iki.fi>
2019-01-26 13:35:15 +0200

    Some base structure for generating a map.

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Generator : MonoBehaviour
+{
+    public GameObject groundTilemap_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void generateScenario()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
