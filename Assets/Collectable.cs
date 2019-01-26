b5ab466634c91c81ce8ed831973733a7b9892082
Joni Räsänen <joni.rasanen@iki.fi>
2019-01-26 23:27:06 +0200

    Make collectible script. How do I get the reference to gameMaster? Tag.

---
+++ Assets/Collectable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    public int resourceAmount_ = 0;
+    public int id;
+
+    public gameMaster master_;
+    public SpriteRenderer renderer_;
+
+    public Sprite usedSprite_;
+
+    public void collect()
+    {
+        if (resourceAmount_ == 0)
+        {
+            Debug.Log("ERROR: no resource amount set for collectable");
+            return;
+        }
+
+        --resourceAmount_;
+        master_.addResource(id);
+
+        if (resourceAmount_ == 0)
+        {
+            renderer_.sprite = usedSprite_;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
