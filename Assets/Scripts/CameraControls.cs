2842bbbdc984b3a509f3ba6456635d8f55f881c8
Joni Räsänen <joni.rasanen@iki.fi>
2019-01-27 14:11:37 +0200

    Some camera limits.

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -8,25 +8,41 @@ public class CameraControls : MonoBehaviour
 
     public int cameraSpeed_ = 10;
 
+    private int levelWidth = 64;
+    private int levelHeight = 64;
+
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(transform.position);
         if (Input.GetKey("up") || Input.GetKey("w"))
         {
-            transform.position += new Vector3(0, Time.deltaTime * cameraSpeed_, 0);
+            if (transform.position.y + Time.deltaTime * cameraSpeed_ < levelHeight / 2)
+            {
+                transform.position += new Vector3(0, Time.deltaTime * cameraSpeed_, 0);
+            }
         }
         if (Input.GetKey("down") || Input.GetKey("s"))
         {
-            transform.position += new Vector3(0, -Time.deltaTime * cameraSpeed_, 0);
+            if (transform.position.y + Time.deltaTime * cameraSpeed_ > 0)
+            {
+                transform.position += new Vector3(0, -Time.deltaTime * cameraSpeed_, 0);
+            }
         }
         if (Input.GetKey("left") || Input.GetKey("a"))
         {
-            transform.position += new Vector3(-Time.deltaTime * cameraSpeed_, 0, 0);
+            if (transform.position.x + Time.deltaTime * cameraSpeed_ > -levelWidth / 2)
+            {
+                transform.position += new Vector3(-Time.deltaTime * cameraSpeed_, 0, 0);
+            }
         }
         if (Input.GetKey("right") || Input.GetKey("d"))
         {
-            transform.position += new Vector3(Time.deltaTime * cameraSpeed_, 0, 0);
+            if (transform.position.x + Time.deltaTime * cameraSpeed_ < levelWidth / 2)
+            {
+                transform.position += new Vector3(Time.deltaTime * cameraSpeed_, 0, 0);
+            }
         }
     }
 }
