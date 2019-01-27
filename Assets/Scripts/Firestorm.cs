c91b6fb698e9b07fc23107f5ca65c148e49db171
Ormanus <osalminenster@gmail.com>
2019-01-27 14:54:58 +0200

    I am fire, I am death!

---
+++ Assets/Scripts/Firestorm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firestorm : MonoBehaviour
+{
+    public ParticleSystem ps;
+    public Camera mainCamera;
+    void Start()
+    {
+        float height = 2f * mainCamera.orthographicSize;
+        float width = height * mainCamera.aspect;
+
+        ps = GetComponent<ParticleSystem>();
+        var shape = ps.shape;
+        shape.scale = new Vector3(width, height, 1f);
+
+        ALL_CONSUMING_INFERNO();
+    }
+
+    public void ALL_CONSUMING_INFERNO()
+    {
+        ps.Play();
+    }
+}
